fc729a11a6d9da82117d812a61c0b022bfce9d28
choang <charhoang@outlook.com>
2022-11-15 22:57:34 +0000

    output

--- ascii-output1.go
+++ ascii-output1.go
@@ -3,29 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
 
+var j = len(os.Args) - 1
+
+// readFile checks os.Args for the font
 func readFile() string {
-	//for standard, when --output is not specified, terminal prints args[3] (the font name) instead of string
-	//when font is anything else, an error occurs as there is no args[3] when output is not specified
-	//therefore we need to make separate code for when there are 3 args vs when there are only 2
-	if string(os.Args[2]) == "standard" || string(os.Args[3]) == "standard" {
-		text, err := os.ReadFile("standard.txt")
+	if string(os.Args[j]) == "thinkertoy" {
+		text, err := os.ReadFile("thinkertoy.txt")
 		if err != nil {
 			panic(err)
 		}
 		return string(text)
-	} else if string(os.Args[2]) == "shadow" || string(os.Args[3]) == "shadow" {
+	} else if string(os.Args[j]) == "shadow" {
 		text, err := os.ReadFile("shadow.txt")
 		if err != nil {
 			panic(err)
 		}
 		return string(text)
 	} else {
-		text, err := os.ReadFile("thinkertoy.txt")
+		text, err := os.ReadFile("standard.txt")
 		if err != nil {
 			panic(err)
 		}
@@ -33,53 +32,112 @@ func readFile() string {
 	}
 }
 
-func main() {
-	lines := strings.Split(string(readFile()), "\n")
+// code ignores checks to the output flag
+// hence code sees our string input as os.Args[2] for all terminal arguments
+// how to check whether os.Args[1] is a flag?
+func outputChecker() bool {
+	if os.Args[1] == "--output=" {
+		return true
+	} else {
+		return false
+	}
+}
 
+func main() {
 	//OUTPUT FLAG
 	output := flag.String("output", "banner.txt", "output to banner.txt?") //when false, output does nothing
 	flag.Parse()
 
-	//when output is true
-	if *output == "banner.txt" {
-		split := strings.Split(os.Args[2], `\n`)
-		//create banner.txt file
-		_, err := os.Create("banner.txt")
-		if err != nil {
-			panic(err)
-		}
-
-		s := ""
-		for i := 0; i < len(split); i++ {
-			if string(split[i]) == "" {
-				fmt.Println()
-			} else {
-				for j := 0; j < 8; j++ {
-					for k := 0; k < len(string(split[i])); k++ {
-						fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
-						s += lines[int(((rune(split[i][k])-32)*9+1))+j]
+	switch os.Args[j] {
+	//THINKERTOY
+	case "thinkertoy":
+		lines := strings.Split(string(readFile()), "\r\n") //thinkertoy NEEDS a carriage return
+		if outputChecker() {                               //when thinkertoy && output == true, strings input will be os.Args[2]
+			split := strings.Split(os.Args[2], `\r\n`)
+			for i := 0; i < len(split); i++ {
+				if string(split[i]) == "" {
+					fmt.Println()
+				} else {
+					for j := 0; j < 8; j++ {
+						for k := 0; k < len(string(split[i])); k++ {
+							fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
+						}
+						fmt.Print("\r\n")
 					}
-					fmt.Print("\n")
-					s += "\n"
 				}
 			}
-			err = ioutil.WriteFile("banner.txt", []byte(s), 0644)
+		} else { //when thinkertoy && output == false, string input is os.Args[1]
+			split := strings.Split(os.Args[2], `\r\n`)
+			//create banner.txt file
+			_, err := os.Create("banner.txt")
 			if err != nil {
 				panic(err)
 			}
-		}
-	} else {
-		//when output is false therefore os.Args[1] = string
-		split := strings.Split(os.Args[1], `\n`)
-		for i := 0; i < len(split); i++ {
-			if string(split[i]) == "" {
-				fmt.Println()
-			} else {
-				for j := 0; j < 8; j++ {
-					for k := 0; k < len(string(split[i])); k++ {
-						fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
+			s := ""
+			for i := 0; i < len(split); i++ {
+				if string(split[i]) == "" {
+					fmt.Println()
+				} else {
+					for j := 0; j < 8; j++ {
+						for k := 0; k < len(string(split[i])); k++ {
+							fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
+							s += lines[int(((rune(split[i][k])-32)*9+1))+j]
+						}
+						fmt.Print("\r\n")
+						s += "\r\n"
 					}
-					fmt.Print("\n")
+				}
+				err = os.WriteFile("banner.txt", []byte(s), 0644)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+
+	// shadow and standard still require editing, will return to default once thinkertoy is sorted out
+	//STANDARD & SHADOW
+	default:
+		lines := strings.Split(string(readFile()), "\n")
+		switch *output == "banner.txt" { //when output == true
+		case true:
+			split := strings.Split(os.Args[2], `\n`)
+			//create banner.txt file
+			_, err := os.Create("banner.txt")
+			if err != nil {
+				panic(err)
+			}
+			s := ""
+			for i := 0; i < len(split); i++ {
+				if string(split[i]) == "" {
+					fmt.Println()
+				} else {
+					for j := 0; j < 8; j++ {
+						for k := 0; k < len(string(split[i])); k++ {
+							fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
+							s += lines[int(((rune(split[i][k])-32)*9+1))+j]
+						}
+						fmt.Print("\n")
+						s += "\n"
+					}
+				}
+				err = os.WriteFile("banner.txt", []byte(s), 0644)
+				if err != nil {
+					panic(err)
+				}
+			}
+		//when output == false
+		default:
+			split := strings.Split(os.Args[1], `\n`)
+			for i := 0; i < len(split); i++ {
+				if string(split[i]) == "" {
+					fmt.Println()
+				} else {
+					for j := 0; j < 8; j++ {
+						for k := 0; k < len(string(split[i])); k++ {
+							fmt.Print(lines[int(((rune(split[i][k])-32)*9+1))+j])
+						}
+						fmt.Print("\n")
+					}
 				}
 			}
 		}
